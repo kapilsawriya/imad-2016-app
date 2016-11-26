66f26fa71b8f04c9614176813829c7e82873fbc4
kapilsawriya <kapilsawriya@gmail.com>
2016-11-26 19:11:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,39 +1,26 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
 
-var app = express();
-app.use(morgan('combined'));
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
 
-app.get('/article-one',function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));  })
-;
-
-app.get('/article-two',function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));  
-    
-})
-;
-
-app.get('/article-three',function (req, res) {
-    res.send('article three requested and will be served');  })
-;
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+$(document).ready(main);
